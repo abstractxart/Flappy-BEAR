6f7627b7a811957544ff0082fac144303292408b
Your Name <abstractxart@gmail.com>
2025-11-21 15:31:04 -0700

    Align game to top - use CENTER_HORIZONTALLY instead of CENTER_BOTH

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ const config: Phaser.Types.Core.GameConfig = {
   },
   scale: {
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
+    autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
     width: screenSize.width.value,
     height: screenSize.height.value,
     fullscreenTarget: 'game-container',
